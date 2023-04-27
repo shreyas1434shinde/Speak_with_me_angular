a92608296ddc012707f5f86d1512ac37a612470f
TTPL-RT-52 <shreyas1434shinde@gmail.com>
2023-04-27 14:18:59 +0530

    speak with me in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import $ from 'jquery'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+  
 })
+
 export class AppComponent {
-  title = 'y';
+  title = 'speak-with-me';
+
+  ngOnInit() {
+    $(document).ready(function() {
+      $('.btnNext').click(function(){
+        $('.active').next('.nav-link').trigger('click');
+        });
+    });
+ }
 }
